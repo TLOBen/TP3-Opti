ce67b62a69c9c2489130b13d1c6735fb887eefc2
TLOBen <benjamin.dagouret@orange.fr>
2018-03-12 14:01:46 -0400

    init commit

---
+++ Exercice 2/src/fr/uqac/main/Main.java
@@ -0,0 +1,15 @@
+package fr.uqac.main;
+
+/**
+ * Main class
+ * 
+ * @author Julien CUSSET, Benjamin DAGOURET
+ */
+public class Main {
+    /**
+     * @param args Arguments données en ligne de commande
+     */
+    public static void main(String[] args) {
+        
+    }
+}

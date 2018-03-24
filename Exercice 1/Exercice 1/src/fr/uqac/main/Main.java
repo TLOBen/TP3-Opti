83547c2a49e959ed3f4168efc564fa5f017ca7cc
TLOBen <benjamin.dagouret@orange.fr>
2018-03-24 15:24:23 -0400

    Exercice 1 fini

---
+++ Exercice 1/Exercice 1/src/fr/uqac/main/Main.java
@@ -0,0 +1,18 @@
+package fr.uqac.main;
+
+import fr.uqac.simulation.Simulation;
+
+/**
+ * Main class
+ * 
+ * @author Julien CUSSET, Benjamin DAGOURET
+ */
+public class Main {
+    /**
+     * @param args Arguments données en ligne de commande
+     */
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation(4, 6);
+        simulation.simulate();
+    }
+}
---
+++ Exercice 1/Exercice 1/src/fr/uqac/struct/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uqac.struct;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Client {
+    public double arrivee;
+    public double tempsAttente;
+    public double tempsService;
+    
+    public Client(double arrivee) {
+        this.arrivee = arrivee;
+    }
+}
